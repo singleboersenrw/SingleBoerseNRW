9c75643a1167731fcdb1c6012d6814815656139b
Florian-Fynn Scheibel <flori.schei@bkgl.bgl.schule>
2022-09-03 21:16:35 +0200

    Entwicklungsstand03_09_2022-21:16

--- SingleBoerseNRW/MVVM/ViewModel/MainViewModel.cs
+++ SingleBoerseNRW/MVVM/ViewModel/MainViewModel.cs
@@ -1,26 +1,31 @@
 ﻿using SingleBoerseNRW.Core;
+using SingleBoerseNRW.MVVM.ViewModel;
 using System;
 
 
 namespace SingleBoerseNRW.MVVM.View
 {
-    private object _currentView;
-
-    public object CurrentView
-    {
-        get { return _curentView; }
-        set 
-        { 
-            _currentView = value;
-            OnPropertyChanged();
-        }
-    }
-
+   
     class MainViewModel : ObservableObject
+        
     {
+        public HomeViewModel HomeVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainViewModel()
         {
-
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
 
         }
     }
